48f161a081f3a575a151b9e84a3d74a2cda240f8
Allen Bargi <allen.bargi@gmail.com>
2016-06-29 23:12:49 +0200

    3102

--- Contents/MacOS/Packages/Default/profile.py
+++ Contents/MacOS/Packages/Default/profile.py
@@ -1,13 +1,18 @@
 import sublime, sublime_plugin
 
+def profile_text():
+    output = ""
+    for event in sorted(sublime_plugin.profile.keys()):
+        output += "{0}:\n".format(event)
+        for name, summary in sublime_plugin.profile[event].items():
+            output += "    {0}: {1}\n".format(name, summary)
+        output += "\n"
+    return output
+
 class ProfilePluginsCommand(sublime_plugin.WindowCommand):
     def run_(self, edit_token, args):
         output = "This list shows how much time each plugin has taken to respond to each event:\n\n"
-        for event in sorted(sublime_plugin.profile.keys()):
-            output += "{0}:\n".format(event)
-            for name, summary in sublime_plugin.profile[event].items():
-                output += "    {0}: {1}\n".format(name, summary)
-            output += "\n"
+        output += profile_text()
 
         v = self.window.new_file()
         v.set_scratch(True)
@@ -15,4 +20,3 @@ class ProfilePluginsCommand(sublime_plugin.WindowCommand):
         edit = v.begin_edit(edit_token, "")
         v.insert(edit, 0, output)
         v.end_edit(edit)
-
